c429383c4a1520adf2d91968b49ddecf55c8ad62
Ahmed Soliman <ahmed.soliman@programmer.net>
2016-12-06 23:20:24 -0800

    coded the ANgular controller for services and assigned to call services.json

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
 var app = angular.module('footerLinks', [])
 
+app.controller('servicesCtrl', ['$scope', '$http', function($scope, $http) {
+	$http.get('service.json').then(function(response) {
+		$scope.services = response.data
+	})
+}])
 
 }])
