8cc71c2b3a5709227df5d036fc8559649b106d73
Ahmed Soliman <ahmed.soliman@programmer.net>
2016-12-07 23:28:16 -0800

    orgnized my code for better reading

--- js/animation.js
+++ js/animation.js
@@ -1,16 +1,20 @@
 $(document).ready(function() {
 
-$('.nav').addClass('magictime puffIn')
-$('.info-box').css('opacity', '0.0');
-
-$(window).scroll(function() {
+	$('.nav').addClass('magictime puffIn')
+	$('.info-box').css('opacity', '0.0');
+	
+	$(window).scroll(function() {
 		if ($(this).scrollTop() > 20) {
 			$('.info-bg').addClass('magictime slideUp')
 		}
 	})
+	
 	$(window).scroll(function() {
 		if ($(this).scrollTop() > 100) {
 			$('.info-box').addClass('magictime vanishIn')
 		}
 	})
+	
+	
+	
 })
