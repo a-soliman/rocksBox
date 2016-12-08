ba43a97f1d25db8e4c880f59abf9aaa9ea1ec090
Ahmed Soliman <ahmed.soliman@programmer.net>
2016-12-07 23:26:41 -0800

    coded a scroll function to animate the box image

--- js/animation.js
+++ js/animation.js
@@ -3,5 +3,9 @@ $(document).ready(function() {
 $('.nav').addClass('magictime puffIn')
 $('.info-box').css('opacity', '0.0');
 
-
+$(window).scroll(function() {
+		if ($(this).scrollTop() > 20) {
+			$('.info-bg').addClass('magictime slideUp')
+		}
+	})
 })
